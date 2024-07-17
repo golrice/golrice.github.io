839000098832da022b59f77b0371d1394ee098c3
root <root@LAPTOP-CG84UU5A>
2024-07-17 19:45:48 +0800

    add local storage

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 function addTodo() {
-    var input = document.getElementById('todoInput');
-    var newTodo = input.value.trim();
-    if (newTodo !== '') {
-        var todoList = document.getElementById('todoList');
-        var li = document.createElement('li');
-        li.className = 'todo-item';
-        li.textContent = newTodo;
-        li.onclick = function () {
-            this.parentNode.removeChild(this);
-        };
-        todoList.appendChild(li);
-        input.value = ''; // Clear input field
+    const input = document.getElementById('todoInput');
+    const newTodo = input.value.trim();
+
+    if (newTodo === '') {
+        return;
     }
+
+    const alreadyExists = localStorage.getItem('todos');
+    if (alreadyExists && alreadyExists.includes(newTodo)) {
+        alert('Todo already exists!');
+        return;
+    }
+
+    createTodo(newTodo);
+
+    saveTodo(newTodo);
+
+    input.value = ''; // Clear input field
+}
+
+function createTodo(content) {
+    const newTodo = document.createElement(content);
+    newTodo.className = 'todo-item';
+    newTodo.textContent = content;
+    newTodo.onclick = () => {
+        this.parentNode.removeChild(this);
+    };
+
+    const todoList = document.getElementById('todoList');
+    todoList.appendChild(newTodo);
+
+    return newTodo;
+}
+
+function saveTodo(newContent) {
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
+    todos.push(newContent);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     var items = JSON.parse(localStorage.getItem('todos')) || [];
-    items.forEach(function (item) {
-        addTodo(item);
+    items.forEach((item) => {
+        createTodo(item);
     });
 });
